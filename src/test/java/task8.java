0ea8638327552f4388d85298bf8f7c22e81da98c
Pavel <pkinyapin+github@gmail.com>
2017-02-03 16:32:27 +0200

    Code for tsk 8 - Сделайте сценарий, проверяющий наличие стикеров у товаров

---
+++ src/test/java/task8.java
@@ -0,0 +1,51 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+/**
+ * Created by KPR on 03/02/2017.
+ * Сделайте сценарий, проверяющий наличие стикеров у товаров
+ */
+
+
+public class task8 {
+    private WebDriver driver;
+
+    @Before
+    public void Start()
+    {
+    driver = new ChromeDriver();
+    }
+
+    @Test
+    public void myTest() throws Exception
+    {
+        // open mainpage of shop
+        driver.get("http://localhost/litecart/");
+
+        // creating a list of products on mainpage
+        List<WebElement> elements = driver.findElements(By.cssSelector("li[class^=product]"));
+
+        // checking of each elemnt have only one sticker
+        for (WebElement el :elements)
+        {
+            List<WebElement> stickers = el.findElements(By.cssSelector("[class^=sticker]"));
+            Assert.assertTrue(el.findElements(By.cssSelector("[class^=sticker]")).size() == 1);
+        }
+    }
+
+    @After
+    public void Stop()
+    {
+        driver.quit();
+        driver = null;
+    }
+
+}
