5547520acc3bcf56d5f231afa792e6e6d953d232
Pavel <pkinyapin@gmail.com>
2017-02-13 13:34:29 +0200

    Update task 10
    removing an unusable imports

--- src/test/java/task10.java
+++ src/test/java/task10.java
@@ -6,12 +6,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
