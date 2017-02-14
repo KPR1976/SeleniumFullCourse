7413640208867ec102de109b345ef32c5bd9d36a
Pavel <pkinyapin@gmail.com>
2017-02-14 18:42:44 +0200

    refactoring code for task 13: adding loop for adding to cart

--- src/test/java/task13.java
+++ src/test/java/task13.java
@@ -38,41 +38,32 @@ public class task13
 
     @Test
     public void myTest() throws Exception {
-        // open mainpage of shop
-        driver.get("http://localhost/litecart/");
-        // find first item in popular and click
-        driver.findElement(By.cssSelector("#box-most-popular li:nth-child(1)")).click();
-        if (driver.findElement(By.tagName("h1")).getAttribute("textContent").equals("Yellow Duck"))
-        new Select(driver.findElement(By.cssSelector("[name='options[Size]']"))).selectByValue("Large");
-        driver.findElement(By.cssSelector(".quantity button")).click();
-        wait.until(textToBePresentInElementLocated(By.cssSelector("#cart .quantity"),"1"));
-
-        driver.get("http://localhost/litecart/");
-        // find first item in popular and click
-        driver.findElement(By.cssSelector("#box-most-popular li:nth-child(1)")).click();
-        if (driver.findElement(By.tagName("h1")).getAttribute("textContent").equals("Yellow Duck"))
-        new Select(driver.findElement(By.cssSelector("[name='options[Size]']"))).selectByValue("Large");
-        driver.findElement(By.cssSelector(".quantity button")).click();
-        wait.until(textToBePresentInElementLocated(By.cssSelector("#cart .quantity"),"2"));
-
-        driver.get("http://localhost/litecart/");
-        // find first item in popular and click
-        driver.findElement(By.cssSelector("#box-most-popular li:nth-child(1)")).click();
-        if (driver.findElement(By.tagName("h1")).getAttribute("textContent").equals("Yellow Duck"))
-        new Select(driver.findElement(By.cssSelector("[name='options[Size]']"))).selectByValue("Large");
-        driver.findElement(By.cssSelector(".quantity button")).click();
-        wait.until(textToBePresentInElementLocated(By.cssSelector("#cart .quantity"),"3"));
-
+        // open mainpage of shop and add 3 ducks to cart in loop
+        for (int i = 1; i <= 3; i++) {
+            String num = String.valueOf(i);
+            // open mainpage
+            driver.get("http://localhost/litecart/");
+            // find first item in popular and click
+            driver.findElement(By.cssSelector("#box-most-popular li:nth-child(1)")).click();
+            // check "Yellow duck" because it contains 3 sizes of duck
+            if (driver.findElement(By.tagName("h1")).getAttribute("textContent").equals("Yellow Duck"))
+                new Select(driver.findElement(By.cssSelector("[name='options[Size]']"))).selectByValue("Large");
+            // add the ducj to cart
+            driver.findElement(By.cssSelector(".quantity button")).click();
+            // wait for renew of quantity in cart
+            wait.until(textToBePresentInElementLocated(By.cssSelector("#cart .quantity"),num));
+        }
+        // go to checkout
         driver.findElement(By.cssSelector("#cart .link")).click();
-
+        // count how elements in cart and remove one after one
         List<WebElement> row = driver.findElements(By.cssSelector("#order_confirmation-wrapper tr td.item"));
         for (int i = 0; i < row.size(); i++)
         {
+
             WebElement removeitem = driver.findElement(By.cssSelector("[value='Remove']"));
             removeitem.click();
             wait.until(stalenessOf(row.get(0)));
         }
-
     }
 
     @After
